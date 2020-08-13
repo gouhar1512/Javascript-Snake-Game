a13f69225a0f3762d4dc0f04e863332f5924bb29
gouhar1512 <ushran987@gmail.com>
2020-08-14 02:38:59 +0530

    Added buttons

--- grid.js
+++ grid.js
@@ -1,15 +1,18 @@
-const GRID_SIZE = 21
+const GRID_WIDTH = 19;
+const GRID_HEIGHT = 29;
 
 export function randomGridPosition() {
   return {
-    x: Math.floor(Math.random() * GRID_SIZE) + 1,
-    y: Math.floor(Math.random() * GRID_SIZE) + 1
-  }
+    x: Math.floor(Math.random() * GRID_WIDTH) + 1,
+    y: Math.floor(Math.random() * GRID_HEIGHT) + 1,
+  };
 }
 
 export function outsideGrid(position) {
   return (
-    position.x < 1 || position.x > GRID_SIZE ||
-    position.y < 1 || position.y > GRID_SIZE
-  )
+    position.x < 1 ||
+    position.x > GRID_WIDTH ||
+    position.y < 1 ||
+    position.y > GRID_HEIGHT
+  );
 }
--- input.js
+++ input.js
@@ -1,28 +1,44 @@
-let inputDirection = { x: 0, y: 0 }
-let lastInputDirection = { x: 0, y: 0 }
+let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
+let leftBtn = document.getElementsByClassName("left-btn")[0];
+leftBtn.addEventListener("click", () => {
+  moveSnake("Left");
+});
+let rightBtn = document.getElementsByClassName("right-btn")[0];
+rightBtn.addEventListener("click", () => {
+  moveSnake("Right");
+});
+let upBtn = document.getElementsByClassName("up-btn")[0];
+upBtn.addEventListener("click", () => {
+  moveSnake("Up");
+});
+let downBtn = document.getElementsByClassName("down-btn")[0];
+downBtn.addEventListener("click", () => {
+  moveSnake("Down");
+});
 
-window.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'ArrowUp':
-      if (lastInputDirection.y !== 0) break
-      inputDirection = { x: 0, y: -1 }
-      break
-    case 'ArrowDown':
-      if (lastInputDirection.y !== 0) break
-      inputDirection = { x: 0, y: 1 }
-      break
-    case 'ArrowLeft':
-      if (lastInputDirection.x !== 0) break
-      inputDirection = { x: -1, y: 0 }
-      break
-    case 'ArrowRight':
-      if (lastInputDirection.x !== 0) break
-      inputDirection = { x: 1, y: 0 }
-      break
+function moveSnake(dir) {
+  switch (dir) {
+    case "Up":
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: -1 };
+      break;
+    case "Down":
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: 1 };
+      break;
+    case "Left":
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: -1, y: 0 };
+      break;
+    case "Right":
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: 1, y: 0 };
+      break;
   }
-})
+}
 
 export function getInputDirection() {
-  lastInputDirection = inputDirection
-  return inputDirection
+  lastInputDirection = inputDirection;
+  return inputDirection;
 }
